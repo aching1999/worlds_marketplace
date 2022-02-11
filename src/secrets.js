66e2f81d85842f18afe0bfb97158048aa7b30cb9
Kenny Rosas-Mondragon <51158638+vtsxking@users.noreply.github.com>
2022-02-11 01:38:38 -0600

    Delete secrets.js

--- src/secrets.js
+++
@@ -1,8 +0,0 @@
-const PROJECT_ID="mainnet3eI2HGY9oDPZkQveItihr5lIP94sWWtF"
-// const PROJECT_ID="testnetL0riA5tMipy3cQ45FjldsEdTSTuClr8V"
-
-let secrets = {
-    PROJECT_ID: PROJECT_ID
-}
-
-export default secrets;
