03ecdc30dfd12dfd6e983aa53388f4fcdeb47526
Kenny Rosas-Mondragon <51158638+vtsxking@users.noreply.github.com>
2022-02-11 01:29:57 -0600

    Create secrets.js

---
+++ src/secrets.js
@@ -0,0 +1,8 @@
+const PROJECT_ID="mainnet3eI2HGY9oDPZkQveItihr5lIP94sWWtF"
+// const PROJECT_ID="testnetL0riA5tMipy3cQ45FjldsEdTSTuClr8V"
+
+let secrets = {
+    PROJECT_ID: PROJECT_ID
+}
+
+export default secrets;
